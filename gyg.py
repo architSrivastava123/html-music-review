56284074acf9678126d97c307dd9b2fd7ebc2c3d
architSrivastava123 <149877302+architSrivastava123@users.noreply.github.com>
2024-01-24 13:13:20 +0530

    Create gyg.py

---
+++ gyg.py
@@ -0,0 +1,10 @@
+print(" wleocme to ticket box")
+age=int(input("enter your age"))
+
+if age> 18:
+  print("you can't buy a ticket sorry came after you become 18+" )
+else:
+  print (" you can book a ticket ")
+  
+
+      
